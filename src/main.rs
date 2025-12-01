b3aae12c36c6d569cd8446634ec061d491317b4c
JGM01 <jacobgermanamccray@gmail.com>
2025-11-30 21:42:52 -0500

    connection made

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use tonic::{Request, Response, Status, transport::Server};
+
+use crate::wubrag::{
+    FilePath, IndexResult,
+    wub_rag_server::{WubRag, WubRagServer},
+};
+
+pub mod wubrag {
+    tonic::include_proto!("wubrag");
+}
+
+#[derive(Debug, Default)]
+pub struct Wub {}
+
+#[tonic::async_trait]
+impl WubRag for Wub {
+    async fn index(&self, request: Request<FilePath>) -> Result<Response<IndexResult>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = IndexResult {
+            ok: true,
+            err: "".to_string(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 5001);
+    let rag = Wub::default();
+    Server::builder()
+        .add_service(WubRagServer::new(rag))
+        .serve(socket)
+        .await?;
+
+    Ok(())
 }
